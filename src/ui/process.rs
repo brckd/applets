ba29511b7d77704d46b480dd78ee9038390a4bbd
bricked <git@bricked.dev>
2025-04-01 22:21:57 +0200

    feat: add info tab

--- src/ui/process.rs
+++ src/ui/process.rs
@@ -1,11 +1,12 @@
-use relm4::{ComponentParts, ComponentSender, Component, RelmWidgetExt, SimpleComponent};
+use relm4::{tokio::task, Component, ComponentParts, ComponentSender, RelmWidgetExt, SimpleComponent};
 use adw::prelude::*;
 use std::{str, io::{BufReader, BufRead}, os::unix::thread, process::{Command,Stdio}, time::Duration};
 
 pub struct ProcessModel {
     visible: bool,
+    info: gtk::TextBuffer,
     output: gtk::TextBuffer,
-    errors: gtk::TextBuffer,
+    errors: gtk::TextBuffer
 }
 
 #[derive(Debug)]
@@ -17,8 +18,9 @@ pub enum ProcessMsg {
 
 #[derive(Debug)]
 pub enum CommandMsg {
+    StreamInfo(String),
     StreamOutput(String),
-    StreamErrors(String),
+    StreamErrors(String)
 }
 
 #[relm4::component(pub)]
@@ -37,6 +39,19 @@ impl Component for ProcessModel {
             set_visible: model.visible,
             
             gtk::Notebook {
+                append_page[Some::<&gtk::Label>(&gtk::Label::new(Some("Info")))] = &gtk::Box {
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_spacing: 5,
+                    set_margin_all: 5,
+        
+                    gtk::TextView {
+                        set_vexpand: true,
+    
+                        #[watch]
+                        set_buffer: Some(&model.info),
+                        set_editable: false,
+                    }
+                },
                 append_page[Some::<&gtk::Label>(&gtk::Label::new(Some("Output")))] = &gtk::Box {
                     set_orientation: gtk::Orientation::Vertical,
                     set_spacing: 5,
@@ -72,7 +87,9 @@ impl Component for ProcessModel {
                             set_editable: false,
                         }
                     }
-                }
+                },
+
+                set_current_page: Some(1)
             }
 
 
@@ -85,7 +102,7 @@ impl Component for ProcessModel {
         root: Self::Root,
         sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let model = ProcessModel { visible: false, output: gtk::TextBuffer::default(), errors: gtk::TextBuffer::default() };
+        let model = ProcessModel { visible: false, info: gtk::TextBuffer::default(), output: gtk::TextBuffer::default(), errors: gtk::TextBuffer::default() };
         let widgets = view_output!();
         ComponentParts { model, widgets }
     }
@@ -93,35 +110,50 @@ impl Component for ProcessModel {
      fn update(&mut self, msg: Self::Input, sender: ComponentSender<Self>, 
         _: &Self::Root,) {
         match msg {
-            ProcessMsg::Run(command) => {
+            ProcessMsg::Run(input) => {
                 self.output.set_text("");
                 self.errors.set_text("");
                 
-                let parts = command.split_ascii_whitespace().collect::<Vec<&str>>();
+                let parts = input.split_ascii_whitespace().collect::<Vec<&str>>();
                 let name = parts.first().unwrap();
                 let args = &parts[1..];
 
-                let mut primitive_command = Command::new(name);
-                let command = primitive_command.args(args).stdout(Stdio::piped());
-                let mut process = command.spawn().unwrap();
+                let mut command = Command::new(name);
+                let command = command.args(args).stdout(Stdio::piped()).stderr(Stdio::piped());
 
-                sender.spawn_command(move |out| {
-                    if let Some(output) = process.stdout.as_mut() {
-                        for line in BufReader::new(output).lines() {
-                            let line = line.unwrap() + "\n";
-                            // TODO: Implement ANSI escape codes
-                            let line = String::from_utf8(strip_ansi_escapes::strip(line.as_bytes())).unwrap();
-                            out.send(CommandMsg::StreamOutput(line)).unwrap();
-                        }
+                let mut process = command.spawn();
+                match process {
+                    Err(error) => {
+                        self.info.set_text(format!("Error: {}",  &error).as_str());
                     }
+                    Ok(mut process) => {
+                        self.info.set_text(format!("Running: {}", input).as_str());
 
-                    if let Some(errors) = process.stderr.as_mut() {
-                        for line in BufReader::new(errors).lines() {
-                            let line = line.unwrap() + "\n";
-                            out.send(CommandMsg::StreamErrors(line)).unwrap();
-                        }
+                        sender.spawn_command(move |out| {
+                            if let Some(output) = process.stdout.as_mut() {
+                                let mut lines = BufReader::new(output).lines();
+                                for line in lines {
+                                    let line = line.unwrap() + "\n";
+                                    // TODO: Implement ANSI escape codes
+                                    let line = String::from_utf8(strip_ansi_escapes::strip(line.as_bytes())).unwrap();
+                                    out.send(CommandMsg::StreamOutput(line)).unwrap();
+                                };
+                            };
+                        });
+        
+                        sender.spawn_command(move |out| {
+                            if let Some(errors) = process.stderr.as_mut() {
+                                let mut lines = BufReader::new(errors).lines();
+                                for line in lines {
+                                    let line = line.unwrap() + "\n";
+                                    out.send(CommandMsg::StreamErrors(line)).unwrap();
+                                };
+                            };
+                        });
                     }
-                });
+                }
+
+
             }
             ProcessMsg::Show => {
                 self.visible = true;
@@ -139,6 +171,9 @@ impl Component for ProcessModel {
             _: &Self::Root,
         ) {
         match msg {
+            CommandMsg::StreamInfo(text) => {
+                self.info.insert(&mut self.info.iter_at_offset(-1), text.as_str());
+            }
             CommandMsg::StreamOutput(text) => {
                 self.output.insert(&mut self.output.iter_at_offset(-1), text.as_str());
             }
